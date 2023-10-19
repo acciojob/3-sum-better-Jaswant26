1ca279be5f32183a48ef6759cc1281ad279774f7
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-10-19 15:09:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function threeSum(nums, target) {
-  // Sort the input array
+function threeSumClosest(nums, target) {
   nums.sort((a, b) => a - b);
+  let closestSum = nums[0] + nums[1] + nums[2];
+  const n = nums.length;
 
-  let closestSum = Infinity;
-
-  for (let i = 0; i < nums.length - 2; i++) {
+  for (let i = 0; i < n - 2; i++) {
     let left = i + 1;
-    let right = nums.length - 1;
+    let right = n - 1;
 
     while (left < right) {
       const currentSum = nums[i] + nums[left] + nums[right];
+
       if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
         closestSum = currentSum;
       }
@@ -28,5 +28,5 @@ function threeSum(nums, target) {
 // Example usage:
 const S = [-1, 2, 1, -4];
 const target = 1;
-const result = threeSum(S, target);
-console.log(result); // Output: 2
+const result = threeSumClosest(S, target);
+console.log("Closest sum to the target:", result);
